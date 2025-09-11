be450f8fdc516b5d1c72ec785468e0493b34631f
Glennendy Magalhães <glennendy999@gmail.com>
2025-09-11 16:22:02 -0300

    12-tipos-primitivos-pt-03-casting

---
+++ 12-tipos-primitivos-pt-03-casting/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000;
+        double salarioDouble = 2800;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println(caractere);
+    }
+}

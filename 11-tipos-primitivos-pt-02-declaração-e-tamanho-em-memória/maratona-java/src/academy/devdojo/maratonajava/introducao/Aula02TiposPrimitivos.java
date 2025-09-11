645e5b3ff5217424d0ff4f7e56024e8769c37c97
Glennendy Magalhães <glennendy999@gmail.com>
2025-09-11 15:23:49 -0300

    11-tipos-primitivos-declaraçoes-e-tamanho-em-memoria

---
+++ 11-tipos-primitivos-pt-02-declaração-e-tamanho-em-memória/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2800;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println(caractere);
+    }
+}

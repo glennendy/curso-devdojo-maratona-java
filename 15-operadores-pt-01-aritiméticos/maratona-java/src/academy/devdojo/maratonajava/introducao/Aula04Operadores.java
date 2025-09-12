cd0f498b8f1e1336942acc4e7f6473632c13c76f
Glennendy Magalhães <glennendy999@gmail.com>
2025-09-12 15:24:44 -0300

    15-operadores-pt-01-aritiméticos

---
+++ 15-operadores-pt-01-aritiméticos/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // Operadores aritmeticos são: + - / *
+        int numero01 = 20;
+        int numero02 = 40;
+        int resultado = numero01+numero02;
+        System.out.println("O valor é "+(numero01+numero02));
+    }
+}

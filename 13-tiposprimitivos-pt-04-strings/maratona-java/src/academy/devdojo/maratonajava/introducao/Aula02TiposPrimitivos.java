4415cb15f559f830177c1fec2fb7986cc211ffad
Glennendy Magalhães <glennendy999@gmail.com>
2025-09-12 11:07:48 -0300

    13-tiposprimitivos-pt-04-strings

---
+++ 13-tiposprimitivos-pt-04-strings/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000;
+        double salarioDouble = 2800;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+        String nome = "Goku";
+        var nome2 =
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println("Oi meu nome é "+nome);
+    }
+}

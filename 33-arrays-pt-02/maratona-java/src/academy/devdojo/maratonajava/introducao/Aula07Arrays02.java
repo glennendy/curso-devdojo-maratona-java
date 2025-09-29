1c388994d49b55f956f3a311cbd25571c4f9fcbd
Glennendy Magalhães <glennendy999@gmail.com>
2025-09-29 11:24:50 -0300

    33-arrays-pt-02

---
+++ 33-arrays-pt-02/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float, double é 0
+        //Char é \u0000
+        //Boolean é false
+        //String é null
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}

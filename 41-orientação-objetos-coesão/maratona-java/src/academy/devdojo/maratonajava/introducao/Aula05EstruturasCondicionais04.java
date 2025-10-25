0a1a2eaa024c4f6323377391127ebeb402246d8d
Glennendy Magalhães <glennendy999@gmail.com>
2025-10-25 15:22:16 -0300

    41-orientação-objetos-coesão

---
+++ 41-orientação-objetos-coesão/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    //0 a 34,712   9.70%
+    //34,713 a 68,507   37.35%
+    //68.508%    49.50%
+    public static void main(String[] args) {
+        double salarioAnual = 45000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImposto;
+        if (salarioAnual <= 34712){
+            salarioAnual = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual >= 34713 && salarioAnual <= 68507) {
+            salarioAnual = salarioAnual * segundaFaixa;
+        } else {
+            salarioAnual = salarioAnual * terceiraFaixa;
+        }
+        System.out.println(salarioAnual);
+    }
+}
